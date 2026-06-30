f3779b6b1c79774f1ae33604d9e9cc07def072f5
zlf <zlf@itcast.cn>
2026-06-30 14:26:52 +0800

    deleted testEnvironment

--- testEnvironment/src/main/java/com/group26/environment/CheckResult.java
+++
@@ -1,8 +0,0 @@
-package com.group26.environment;
-
-public record CheckResult(String name, String status, String message, long durationMs) {
-
-    public boolean isUp() {
-        return "UP".equals(status);
-    }
-}
