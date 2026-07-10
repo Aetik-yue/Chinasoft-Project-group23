c3de624e56aace472992d7b0ac3e1e1b0caa1154
ymzs-p <mapeilin8w@163.com>
2026-07-10 21:27:26 +0800

    前端小修改

--- backend/src/main/java/com/chinasoft/smokesensor/service/impl/ParrotBehaviorServiceImpl.java
+++ backend/src/main/java/com/chinasoft/smokesensor/service/impl/ParrotBehaviorServiceImpl.java
@@ -23,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
